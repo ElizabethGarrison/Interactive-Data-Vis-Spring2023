12af55713274c310d48154f06c4f379ebe4de628
Elizabeth Garrison <Garrison.Elizabeth3@gmail.com>
2023-02-07 19:14:39 -0500

    Finished

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,15 @@
 console.log('hello world');
+
+console.log(document)
+console.log(window)
+
+const input = document.getElementById("name-input")
+console.log(input)
+const updateName = () =>{
+    console.log('in update function')
+    const userName = input.value;
+    window.alert('Hello, welcome to class ${userName}')
+    
+}
+
+updateName();
