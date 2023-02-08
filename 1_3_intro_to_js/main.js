b3c65ab86442c8986f1432788431a6b248837d1c
Elizabeth Garrison <Garrison.Elizabeth3@gmail.com>
2023-02-07 20:27:17 -0500

    Finished

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -9,7 +9,44 @@ const updateName = () =>{
     console.log('in update function')
     const userName = input.value;
     window.alert('Hello, welcome to class ${userName}')
-    
+
 }
 
-updateName();
+//Example of 
+const dataVizClass = {
+    day: 'Tuesday',
+    time: 'late',
+    students: '15',
+}
+
+const dayAccessor = 'day'
+const day = dataVizClass[dayAccessor]
+//or 
+//dataVizClass['day']
+//dataVizClass.day
+
+console.log('day', day)
+
+const keys = Object.keys(dataVizClass)
+console.log(keys)
+
+const values = Object.values(dataVizClass)
+console.log(values)
+
+const entries = Object.entries(dataVizClass)
+console.log(entries)
+
+
+//Example of date
+const now = new Date()
+
+//Homework
+let changeable = true;
+const constant = true;
+let counter = 0;
+
+function change(){
+    changeable = false;
+    const constant = false;
+    
+}
