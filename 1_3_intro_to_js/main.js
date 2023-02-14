4b8e19b5f822284d7e33be7e828d60edcbc81668
Elizabeth Garrison <Garrison.Elizabeth3@gmail.com>
2023-02-14 13:29:23 -0500

    Finished

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,52 +1,6 @@
-console.log('hello world');
+let counter = 0
 
-console.log(document)
-console.log(window)
-
-const input = document.getElementById("name-input")
-console.log(input)
-const updateName = () =>{
-    console.log('in update function')
-    const userName = input.value;
-    window.alert('Hello, welcome to class ${userName}')
-
-}
-
-//Example of 
-const dataVizClass = {
-    day: 'Tuesday',
-    time: 'late',
-    students: '15',
-}
-
-const dayAccessor = 'day'
-const day = dataVizClass[dayAccessor]
-//or 
-//dataVizClass['day']
-//dataVizClass.day
-
-console.log('day', day)
-
-const keys = Object.keys(dataVizClass)
-console.log(keys)
-
-const values = Object.values(dataVizClass)
-console.log(values)
-
-const entries = Object.entries(dataVizClass)
-console.log(entries)
-
-
-//Example of date
-const now = new Date()
-
-//Homework
-let changeable = true;
-const constant = true;
-let counter = 0;
-
-function change(){
-    changeable = false;
-    const constant = false;
-    
+const addOne = () => {
+	counter = counter + 1
+	document.getElementById("counter").innerHTML = counter;
 }
