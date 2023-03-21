a8a6a1b5bdb17d7a171495b386bc60136b55859a
Elizabeth Garrison <Garrison.Elizabeth3@gmail.com>
2023-03-21 17:41:46 -0400

    Finished

--- 2_2_distributions/main.js
+++ 2_2_distributions/main.js
@@ -1,16 +1,61 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  radius = 5;
 
 /* LOAD DATA */
-d3.json("[PATH_TO_YOUR_DATA]", d3.autoType)
-  .then(data => {
-    console.log(data)
+d3.csv("../data/NASAshuttle.csv", d3.autoType).then(data => {
+  console.log(data)
 
-    /* SCALES */
-    
-    /* HTML ELEMENTS */
-    
-  });
+  /* SCALES */
+  const xScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d.Year), d3.max(data, d => d.Year)])
+    .range([margin.left, width - margin.right]);
+
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.Mission))
+    .range([height - margin.bottom, margin.top])
+    .padding(0.1);
+
+  const colorScale = d3.scaleOrdinal()
+    .domain(["Landing", "Launch", "Death"])
+    .range(["#C0A9B0", "#7880B5", "#69995D"]);
+
+  /* HTML ELEMENTS */
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.format("d"));
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis);
+
+  const yAxis = d3.axisLeft(yScale)
+    .tickSize(0)
+    .tickPadding(10);
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis)
+    .call(g => g.select(".domain").remove());
+
+  const dot = svg.selectAll("circle")
+    .data(data)
+    .join("circle")
+    .attr("cx", d => xScale(d.Year))
+    .attr("cy", d => yScale(d.Mission))
+    .attr("r", radius)
+    .attr("fill", d => colorScale(d.Type))
+    .append("title")
+    .text(d => d.Mission + ": " + d.Type);
+
+});
+
+//NOTE: I wanted the y-axis to list the months while pertaining to the mission to be graphed and having the corresponding color variations; but am unable to get it to work. 
+
+
--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,66 @@
- /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+// set the dimensions and margins of the graph
+const margin = { top: 20, right: 30, bottom: 50, left: 60 },
+  width = 600 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+d3.csv("../data/cats.csv", d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
-  }
+    year: new Date(+d.Year, 0, 1),
+    cats: +d["Number of Cats"]
+  };
 }).then(data => {
-  console.log('data :>> ', data);
+  console.log("data :>> ", data);
 
-  // SCALES
+  /* SCALES */
+  //X Scale
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(data, d => d.year))
+    .range([margin.left, width - margin.right]);
 
-  // CREATE SVG ELEMENT
+  // Y Scale
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, d => d.cats)])
+    .range([height - margin.bottom, margin.top]);
+
+  /* HTML ELEMENTS */
 
   // BUILD AND CALL AXES
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
 
-  // LINE GENERATOR FUNCTION
+  svg.append("g")
+    .attr("transform", `translate(0, ${height - margin.bottom})`)
+    .call(xAxis);
 
-  // DRAW LINE
+  svg.append("g")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(yAxis);
+
+// AREA GENERATOR FUNCTION
+const areaGen = d3.area()
+  .x(d => xScale(d.year))
+  .y0(height - margin.bottom)
+  .y1(d => yScale(d.cats));
+
+// DRAW AREA
+const area = svg
+  .append("path")
+  .datum(data)
+  .attr("class", "area")
+  .attr("d", areaGen)
+  .attr("fill", "lightpink")
+  .attr("stroke", "black");
 
 });
