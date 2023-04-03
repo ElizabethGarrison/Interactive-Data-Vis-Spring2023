568f1d3e473b7c583701c82bd7cda080672bfb4a
Elizabeth Garrison <Garrison.Elizabeth3@gmail.com>
2023-04-03 13:59:03 -0400

    Finished

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,8 +1,7 @@
-
 /* CONSTANTS AND GLOBALS */
 const width = window.innerWidth * .8;
 const height = window.innerHeight *.8 ;
-const margin = 50;
+const margin = 130;
 
 /* LOAD DATA */
 d3.csv('../data/squirrelActivities.csv', d3.autoType)
@@ -13,53 +12,75 @@ d3.csv('../data/squirrelActivities.csv', d3.autoType)
     /* SCALES */
     /** This is where you should define your scales from data to pixel space */
     
-  // xscale - categorical, activity
-
-
-  const xScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d=> d.count)])
-    .range([margin, width - margin]) // visual variable
+    // xscale - linear, count
+    const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d=> d.count)])
+      .range([margin, width - margin]) // visual variable
     
+    // yscale - band, activity
+    const yScale = d3.scaleBand()
+      .domain(data.map(d=> d.activity))
+      .range([height - margin, margin])
+      .paddingInner(.2)
 
-    // yscale - linear,activity
-  const yScale = d3.scaleBand()
-    .domain(data.map(d=> d.activity))
-    .range([height - margin, margin])
-    .paddingInner(.2)
-
-  //color scale - ordinal, colors
-  const color = d3.scaleOrdinal()
-    //.domain(data.map(d=> d.activity))
-    .domain(data)
-    .range(["#011627", "#2ec4b6", "#ff9f1c", "#e9d758", "#cda2ab"])
+    // color scale - ordinal, colors
+    const color = d3.scaleOrdinal()
+      .domain(data.map(d=> d.activity))
+      .range(["#011627", "#2ec4b6", "#ff9f1c", "#e9d758", "#cda2ab"])
 
     /* HTML ELEMENTS */
     /** Select your container and append the visual elements to it */
- // svg
- const svg = d3.select("#container")
- .append("svg")
- .attr("width", width)
- .attr("height", height)
+    
+    // svg
+    const svg = d3.select("#container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-  // bars
-  svg.selectAll("rect")
-    .data(data)
-    .join("rect")
-    .attr("width", d=> xScale(d.count))
-    .attr("height", yScale.bandwidth())
-    .attr("x", margin)
-    .attr("y", d=> yScale(d.activity))
-    .attr("fill", d=> color(d.activity))
+    // bars
+    svg.selectAll("rect")
+      .data(data)
+      .join("rect")
+      .attr("width", d=> xScale(d.count) - margin)
+      .attr("height", yScale.bandwidth())
+      .attr("x", margin)
+      .attr("y", d=> yScale(d.activity))
+      .attr("fill", d=> color(d.activity));
 
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale);
-  
+    // x-axis
+    const xAxis = d3.axisBottom(xScale).ticks(5);
     svg.append("g")
-      .style("transform", `translate(0px, ${height - margin}px)`)
+      .attr("transform", `translate(0, ${height - margin})`)
       .call(xAxis);
+
+    // x-axis label
+    svg.append("text")
+      .attr("class", "axis-label")
+      .attr("text-anchor", "middle")
+      .attr("x", width/2)
+      .attr("y", height - margin/2)
+      .text("Count");
+
+    // y-axis
+    const yAxis = d3.axisLeft(yScale);
     svg.append("g")
-      .style("transform", `translate(${margin}px, 0px)`)
+      .attr("transform", `translate(${margin}, 0)`)
       .call(yAxis);
 
+    // y-axis label
+    svg.append("text")
+      .attr("class", "axis-label")
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .attr("x", -height/2)
+      .attr("y", margin/2)
+      .text("Activity");
 
-  })
+    // title
+    svg.append("text")
+      .attr("class", "chart-title")
+      .attr("x", width/2)
+      .attr("y", margin/2)
+      .text("Squirrel Activities");
+
+  });
--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,22 +1,23 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+height = window.innerHeight * 0.7,
+margin = { top: 20, bottom: 60, left: 60, right: 40 },
+radius = 10;
 
 // // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
+let xScale, yScale;
 
 /* APPLICATION STATE */
 let state = {
-  // data: [],
+  data: [],
 };
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
-  console.log("data", raw_data);
+d3.csv('../data/squirrelActivities.csv', d3.autoType).then(raw_data => {
+  console.log("raw_data", raw_data);
   // save our data to application state
   state.data = raw_data;
+  console.log('state.data')
   init();
 });
 
@@ -24,7 +25,21 @@ d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   /* SCALES */
+  xScale = d3.scaleBand()
+    .domain(state.data.map(d => d.activity))
+    .range([margin.left, width - margin.right])
+    .paddingInner(0.2)
+  
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data, d => d.count)])
+    .range([height - margin.bottom, margin.top])
 
+  //define svg
+  const svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  
 
 
   draw(); // calls the draw function
